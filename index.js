dcb269f00d9ff32601eefc19dc865d0ee71c52d0
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-09-24 23:22:00 +0300

    complete lab 3

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  let text; 
+
+  if (feet <= 400) {
+    text = "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    text = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    text = "I will gladly take your thirty bucks.";
+  } else {
+    text = "No can do.";
+  }
+
+  return text; }
+
+console.log(scuberGreetingForFeet(300));   
+console.log(scuberGreetingForFeet(1500));  
+console.log(scuberGreetingForFeet(2200));  
+console.log(scuberGreetingForFeet(2600));  
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+return city === "NYC" ? "Ok, sounds good.": "No go.";
+} 
+console.log( ternaryCheckCity("NYC"))
+console.log( ternaryCheckCity("Carlifornia"))
+
+
+
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+switch (tip)  {
+  case 'generous':
+            return "Thank you so much.";
+        case 'not as generous':
+            return "Thank you.";
+        default:
+            return "Bye.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+} console.log(switchOnCharmFromTip("generous"))
+console.log (switchOnCharmFromTip("not as generous"))
+console.log(switchOnCharmFromTip("Bye."))
